0ccdb69fa382aee48aecc8095377d82aac9bdf97
daejin-9574 <kimdaejin@bitekps.com>
2025-07-03 23:26:32 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-import {
-  getFirestore,
-  collection,
-  getDocs
-} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAoLoX11dVEOrGSJSUKZI6F23ep5DFZBo0",
-  authDomain: "service-report-22d23.firebaseapp.com",
-  projectId: "service-report-22d23",
-  storageBucket: "service-report-22d23.appspot.com",
-  messagingSenderId: "467709604044",
-  appId: "1:467709604044:web:82be6d0de9289cfd3b9269",
-  measurementId: "G-XYLJ1SS6PD"
-};
+import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+import { app } from "./firebase-init.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
   try {
-    const app = initializeApp(firebaseConfig);
     const db = getFirestore(app);
-    const querySnapshot = await getDocs(collection(db, "reports"));
+    const querySnapshot = await getDocs(collection(db, "report"));
     const statusEl = document.getElementById("server-status");
     if (querySnapshot.size > 0) {
       statusEl.innerHTML = "🟢 서버 연결 성공";
