e33c8b234c67ca4b44bb92d0557497ac922b78d7
daejin-9574 <kimdaejin@bitekps.com>
2025-07-03 23:58:38 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
-import { app } from "./firebase-init.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoLoX11dVEOrGSJSUKZI6F23ep5DFZBo0",
+  authDomain: "service-report-22d23.firebaseapp.com",
+  projectId: "service-report-22d23",
+  storageBucket: "service-report-22d23.appspot.com",
+  messagingSenderId: "467709604044",
+  appId: "1:467709604044:web:82be6d0de9289cfd3b9269",
+  measurementId: "G-XYLJ1SS6PD"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 document.addEventListener("DOMContentLoaded", async () => {
   try {
